66d148f9836f38c5d3c3768d8cf8378968e6acb1
Hovsep Babayan <hovbabayan22@gmail.com>
2026-03-26 13:35:11 -0700

    Update resume PDF

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="mt-5 p-8 flex flex-col gap-5 items-center text-gray-100">
             <p>
               <a
-                href="files/HovsepBResume.pdf"
+                href="files/HovsepBabayanResume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
               >
